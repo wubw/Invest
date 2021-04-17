b66b45f64edbccd1e4b80a181eb4b590ef172635
binweiwu <wubinwei@Wus-MacBook-Pro.local>
2021-04-17 13:43:49 +0800

    refactoring

---
+++ currency_exchange_transactions.py
@@ -0,0 +1,13 @@
+currency_exchange_transactions = []
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':20000, 'time':'2020-9-25', 'rate':1.384377, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':20000, 'time':'2020-9-28', 'rate':1.383182, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':3000, 'time':'2020-9-29', 'rate':1.371, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':20091, 'time':'2020-10-22', 'rate':1.36735, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':15000, 'time':'2020-11-6', 'rate':1.357606, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':20000, 'time':'2020-11-26', 'rate':1.351398, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':26000, 'time':'2020-12-24', 'rate':1.339015, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':19700, 'time':'2021-2-4', 'rate':1.346569, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':30000, 'time':'2021-2-18', 'rate':1.3388, 'place':'dbs'})
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':12999.65625+1, 'time':'2021-3-2', 'rate':1.33125, 'place':'ib'},)
+currency_exchange_transactions.append({'currency':'SGDUSD', 'amount':24969+1, 'time':'2021-3-4', 'rate':1.33168, 'place':'ib'})
+
---
+++ dividends.py
@@ -0,0 +1,26 @@
+dividends = []
+# SIN
+dividends.append({'tick':'C2PU.SI', 'amount':70.8, 'time':'2020-12-11'})
+dividends.append({'tick':'ME8U.SI', 'amount':122.8, 'time':'2020-12-11'})
+dividends.append({'tick':'D05.SI', 'amount':126, 'time':'2020-12-29'})
+dividends.append({'tick':'C2PU.SI', 'amount':71.4, 'time':'2020-12-11'})
+dividends.append({'tick':'ME8U.SI', 'amount':164, 'time':'2021-03-8'})
+dividends.append({'tick':'AJBU.SI', 'amount':134.26, 'time':'2021-03-8'})
+# US
+dividends.append({'tick':'SOXX', 'amount':9.07, 'time':'2021-3-31'})
+dividends.append({'tick':'BABA', 'amount':-1.87, 'time':'2021-4-13'})
+dividends.append({'tick':'SOXX', 'amount':2.07, 'time':'2021-4-1'})
+dividends.append({'tick':'AAPL', 'amount':4.62, 'time':'2021-2-16'})
+dividends.append({'tick':'SPY', 'amount':20.05, 'time':'2021-2-2'})
+dividends.append({'tick':'ARKG', 'amount':56.37, 'time':'2021-1-4'})
+dividends.append({'tick':'ARKK', 'amount':108.14, 'time':'2021-1-4'})
+dividends.append({'tick':'ARKW', 'amount':39.48, 'time':'2021-1-4'})
+dividends.append({'tick':'ARKQ', 'amount':27.02, 'time':'2021-1-4'})
+dividends.append({'tick':'ARKF', 'amount':6.24, 'time':'2021-1-4'})
+dividends.append({'tick':'MSFT', 'amount':9.05, 'time':'2020-12-11'})
+dividends.append({'tick':'AAPL', 'amount':4.62, 'time':'2020-11-13'})
+
+dbs_promotes = []
+dbs_promotes.append({'amount':17.37, 'time':'2021-2-25', 'currency':'USD'})
+dbs_promotes.append({'amount':5.79, 'time':'2020-11-13', 'currency':'USD'})
+
